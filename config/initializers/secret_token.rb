7febb59f60a25dd19627280172274075c2ac18ef
App Academy Student <student@appacademy.io>
2013-10-17 10:29:25 -0700

    Initial commit - rspec setup with skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditClone::Application.config.secret_token = '70feb4673425d5cde08c0053f24e9b2fd59262886b91308fa68d035e32a447b2f513d1610e4be02ecda6bc28d7f72fa14e5fd5cf29519a0874fe161c9618a857'
